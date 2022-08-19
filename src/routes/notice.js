feb53b29c11dabbd9fc97d74a2902818a41df158
JEss6 <38889159+JEss6@users.noreply.github.com>
2022-08-19 09:51:23 +0900

    Sync

---
+++ src/routes/notice.js
@@ -0,0 +1,7 @@
+import pool from '../database/database';
+import serverConfig from '../config/serverConfig';
+import express from 'express';
+
+const router = express.Router();
+
+module.exports = router;
